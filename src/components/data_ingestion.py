2ca0797483e708be204057fad548b630f4d34afa
Ahmad Reshad Adib <adibahmadreshad@gmail.com>
2025-06-24 18:10:52 +0100

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -44,8 +46,15 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_path, test_path = obj.initiate_data_ingestion()
+    
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_path, test_path)
+    train_arr, test_arr, preprocessor_path = data_transformation.initiate_data_transformation(train_path, test_path)
+    
+    model_trainer = ModelTrainer()
+    r2_score = model_trainer.initiate_model_trainer(train_arr, test_arr, preprocessor_path)  # Added preprocessor_path
+    
     print(f"Train data: {train_path}")
     print(f"Test data: {test_path}")
+    print(f"Preprocessor saved at: {preprocessor_path}")
+    print(f"Best model R2 score: {r2_score}")
     
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import dill
 from src.exception import CustomException
+from sklearn.metrics import r2_score
+from src.logger import logging
 
 
 def save_object(obj, file_path):
@@ -23,3 +25,24 @@ def save_object(obj, file_path):
     except Exception as e:
         print(f"Error saving object: {e}")
         raise CustomException(e, sys)   
+    
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    """
+    Evaluate multiple models and return their performance metrics.
+    
+    Parameters:
+    """
+    try:
+        report= {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score= r2_score(y_test, y_test_pred)
+            test_model_score= r2_score(y_train, model.predict(X_train))
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        
+        raise CustomException(e, sys)
